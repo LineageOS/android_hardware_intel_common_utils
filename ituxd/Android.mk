fb5c0daae4aa431eb899849057e130004805f422
Anay Wadhera <awadhera@berkeley.edu>
2021-04-12 00:05:29 -0700

    ituxd: add privapp perms
    Change-Id: Ib3f83038d040d7df20d0a7960d665f5609c3dfdf

--- ituxd/Android.mk
+++ ituxd/Android.mk
@@ -25,26 +25,25 @@ LOCAL_PATH:= $(call my-dir)
 
 # Build
 include $(CLEAR_VARS)
-
 LOCAL_MODULE_TAGS := optional
-
 LOCAL_JNI_SHARED_LIBRARIES := libthermalJNI
-
 LOCAL_REQUIRED_MODULES += libthermalJNI
-
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
-
 LOCAL_PACKAGE_NAME := ituxd
-
 LOCAL_CERTIFICATE := platform
-
 LOCAL_PRIVILEGED_MODULE := true
-
 LOCAL_PROGUARD_ENABLED := disabled
-
 LOCAL_PRIVATE_PLATFORM_APIS := true
-
+LOCAL_REQUIRED_MODULES := privapp_whitelist_com.intel.thermal.xml
 include $(BUILD_PACKAGE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := privapp_whitelist_com.intel.thermal.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
